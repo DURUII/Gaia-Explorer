721a1ac979803a30c8473fc7ba4f9f762872819a
安如衫 <durui@wust.edu.cn>
2024-01-07 21:02:12 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,31 @@
+# global folder paths
 chengdu_dataset = '/Volumes/WD_BLACK/dataset/chengdu'
 xian_dataset = '/Volumes/WD_BLACK/dataset/xian'
 jiaotongsai_dataset = '/Volumes/WD_BLACK/dataset/jiaotongsai'
+
+# geographical information processing parameters
+grid_accuracy = 500
+earth_radius = 6371004
+
+# JIAOTONGSAI DATASET
+process_folder = jiaotongsai_dataset
+
+# data processing parameters
+active_threshold = 25
+
+number_of_workers = 1140
+
+# output file paths
+worker2days_output = '/Volumes/WD_BLACK/dataset/output/worker2days.csv'
+date2workers_output = '/Volumes/WD_BLACK/dataset/output/date2workers.csv'
+worker2days_active_output = '/Volumes/WD_BLACK/dataset/output/worker2days_active.csv'
+
+selected_worker_output = '/Volumes/WD_BLACK/dataset/output/selected_worker.csv'
+selected_task_output = '/Volumes/WD_BLACK/dataset/output/selected_worker.csv'
+
+geographical_bound_output = '/Volumes/WD_BLACK/dataset/output/geographical_bound.csv'
+
+ddf_output = '/Volumes/WD_BLACK/dataset/output/ddf'
+task_output = '/Volumes/WD_BLACK/dataset/output/task.csv'
+
+# additional configuration parameters
